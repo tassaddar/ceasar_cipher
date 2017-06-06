a79cc86223d5cb075b13f5da8599668f01a71022
Oleg <tassaddar-90@yandex.ru>
2017-06-06 14:32:37 +0300

    Create ceasar_cipher.rb

---
+++ ceasar_cipher.rb
@@ -0,0 +1,31 @@
+def ceasar_cipher    #the cipher itself 
+  puts "Your text?"
+  text = gets.chomp
+  
+  puts "The Key?"
+  @key = gets.to_i
+
+def converter word  #helper method which converts a single word
+  key = @key
+split_word = word.split("")
+converted_word = split_word.map! do |letter|
+  if letter == "?" || letter == "." || letter == "," || letter == "!"
+letter = letter
+else
+  letter_to_code = (letter.ord + key).chr
+   end
+ end
+ converted_word.join("")
+ end
+
+converted_text =  text.split(" ")
+
+result =  converted_text.map! do |word|   
+  converter(word) #using the helper method to convert the text word by word
+end
+result.join(" ")
+end
+
+ceasar_cipher
+
+#This is only a general version without z to a cycling maybe add it later
